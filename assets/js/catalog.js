d573f7da9283364958a3c6199c292a8828145c40
sejodoh <teamsejodoh@gmail.com>
2022-08-19 07:54:27 +0700

    fixing landing page and catalog

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -42,30 +42,43 @@ Author: GrayGrids
 
     var catalogObj = [
         {
+            is_exclusive : false,
             img_url: "./assets/images/catalog/1.png",
             preview_url: "https://sejodohdotcom.github.io/widi-agum/#%20"
         },
         {
+            is_exclusive : true,
             img_url: "./assets/images/catalog/2.png",
             preview_url: "https://sejodohdotcom.github.io/arum-agin/"
         },
         {
+            is_exclusive : true,
             img_url: "./assets/images/catalog/3.png",
             preview_url: "https://sejodohdotcom.github.io/riska-tomi/#%20"
         },
         {
+            is_exclusive : true,
             img_url: "./assets/images/catalog/4.png",
             preview_url: "https://sejodohdotcom.github.io/putri-david/"
         }
     ];
 
-    function renderCatalog(data) {
+    function renderCatalog() {
 
-        let renderCatalogElements = data.map((item) => {
+        var checkedPrice = $('.switch-button-checkbox:checked').val() == 'true' ? true : false;
+        var renderObj;
+
+        if(checkedPrice == true) {
+            renderObj = catalogObj.filter(data => data.is_exclusive == false);
+        } else {
+            renderObj = catalogObj.filter(data => data.is_exclusive == true);
+        }
+
+        let renderCatalogElements = renderObj.map((item) => {
 
             return (
 
-                '<div class="col-12 col-sm-4 col-lg-3 mb-5 mb-sm-5"><div class="card text-bg-dark"><img src="'+ item.img_url +'" class="card-img" alt="..."> <div class="card-img-overlay d-flex align-items-end justify-content-center"> <div class="button"> <a class="btn" href="'+ item.preview_url +'" target="_blank">Preview<i class="lni lni-arrow-right"></i></a> </div></div></div></div>'
+                '<div class="col-10 offset-1 offset-sm-0 col-sm-4 col-lg-3 mb-5 mb-sm-5"><div class="card text-bg-dark shadow-cs"><img src="'+ item.img_url +'" class="card-img" alt="..."> <div class="card-img-overlay d-flex align-items-end justify-content-center"> <div class="button"> <a class="btn" href="'+ item.preview_url +'" target="_blank">Preview<i class="lni lni-arrow-right"></i></a> </div></div></div></div>'
 
             )
 
@@ -75,14 +88,12 @@ Author: GrayGrids
 
     };
 
-    renderCatalog(catalogObj);
-    $("#price-selected").on("change", function(){
-        if($('input[name="price_theme"]:checked')){
-            $(this).val("true");
-        }else {
-            $(this).val("false");
-        }
-        console.log($(this).val())
+    renderCatalog();
+
+    $("#price-selected").on('change', function(){
+        renderCatalog();
     });
+
+
 
 })();
